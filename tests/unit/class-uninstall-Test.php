0dfc8abf108f04ccc4febd745e85cdf35bef295f
Brian Henry <BrianHenryIE@gmail.com>
2020-12-04 19:42:07 -0800

    Create class-uninstall-Test.php

---
+++ tests/unit/class-uninstall-Test.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Tests for the root plugin file.
+ *
+ * @package BH_WC_REST_Change_Units
+ * @author  Brian Henry <BrianHenryIE@gmail.com>
+ */
+
+namespace BH_WC_REST_Change_Units;
+
+use BH_WC_REST_Change_Units\woocommerce\Settings_Products;
+
+/**
+ * Class Plugin_WP_Mock_Test
+ */
+class Uninstall_Unit_Test extends \Codeception\Test\Unit {
+
+	/**
+	 * Verify uninstall deletes the option.
+	 *
+	 */
+	public function test_uninstall_delete() {
+
+		global $plugin_root_dir;
+
+		define('WP_UNINSTALL_PLUGIN',true);
+
+		\WP_Mock::userFunction(
+			'delete_option',
+			array(
+				'args'   => Settings_Products::REST_WEIGHT_UNIT_OPTION_ID,
+			)
+		);
+
+		require_once $plugin_root_dir . '/uninstall.php';
+
+	}
+
+
+}
