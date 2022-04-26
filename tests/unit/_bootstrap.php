2c3436dd3bd61c70fb643760f78e0e86eaefaa3a
Brian Henry <BrianHenryIE@gmail.com>
2022-04-26 09:19:39 -0700

    Autoload plugin files during unit tests

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -5,7 +5,8 @@
  * @package    brianhenryie/bh-wc-rest-change-units
  */
 
-//global $plugin_root_dir;
-//require_once $plugin_root_dir . '/autoload.php';
-
+WP_Mock::setUsePatchwork( true );
 WP_Mock::bootstrap();
+
+global $plugin_root_dir;
+require_once $plugin_root_dir . '/autoload.php';
